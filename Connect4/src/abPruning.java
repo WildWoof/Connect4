260a42f9f98e27bcafe63273118e81ad76a641d7
WildWoof <32528182+WildWoof@users.noreply.github.com>
2018-11-25 20:33:36 -0800

    Updated Parameters in aBPruning
    updated the inputs for aB Pruning

--- Connect4/src/abPruning.java
+++ Connect4/src/abPruning.java
@@ -1,24 +1,32 @@
 
 public class abPruning {
-	Board baseState;
+	int depth;
 	
-	public abPruning(Board b) {
-		baseState = new Board(b);
+	public abPruning() {
+		depth = 0;
 	}
 
-	public Square abSearch() {
-		//TODO
+	/**
+	 * MiniMax recursive algorithm. 
+	 * a = the value of best Max, b = value of best Min
+	 * a, b initialized to hugely impossible values so that anything 
+	 * will be accepted over them.
+	 * @param state
+	 * @return
+	 */
+	public Square abSearch(Board state) {
+		Square move = maxValue(state,-1000000000, 1000000000);
 		
 		//Square bestMove = 
+		return move;
+	}
+	
+	public Square maxValue(Board state, int a, int b) {
+		
 		return null;
 	}
 	
-	public int maxValue() {
-		//TODO
-		return 0;
-	}
-	
-	public int minValue() {
+	public int minValue(Board state, int a, int b) {
 		//TODO
 		return 0;
 	}
