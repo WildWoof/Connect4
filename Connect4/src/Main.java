01893a63784abbaaaf2e8e4253db41a9a270308d
coboba123 <cnguyen235@yahoo.com>
2018-11-25 18:28:11 -0800

    Main Menu of User Interface

--- Connect4/src/Main.java
+++ Connect4/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -5,26 +6,69 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		boolean exit = false;
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("4-In-a-Line");
-		System.out.println();
-		System.out.println("What would you like to do?");
-		printOptions();
-		
-		
+		int choice = 0;
 
-		//Board b = new Board();
-		//b.printDisplay();
+		System.out.println("4-In-a-Line");
+		System.out.println("By Richard Matthew Musquiz and Cody Nguyen");
+		System.out.println("----------------------------------------------");
+
+		while (!exit)
+		{
+			System.out.println();
+			System.out.println("What would you like to do?");
+			printOptions();
+
+			choice = scan.nextInt();
+
+
+			switch (choice)
+			{
+			case 1:
+				startNewGame();
+				System.out.println("----------------------------------------------");
+				break;
+
+			case 2:
+				recreateGame();
+				System.out.println("----------------------------------------------");
+				break;
+			case 3:
+				exit = true;
+				System.out.println("----------------------------------------------");
+				break;
+
+			default:
+				System.out.println("Invalid Input, Try Again");
+				System.out.println("----------------------------------------------");
+				break;
+			}
+
+		}
+		System.out.println("Thank You for Playing!");
+
+		// Board b = new Board();
+		// b.printDisplay();
 
 	}
-	
+
 	private static void printOptions()
 	{
 		System.out.println("1.) Start a New Game");
 		System.out.println("2.) Recreate a game and Resume");
-		
-		
+		System.out.println("3.) Exit");
+
+	}
+
+	private static void startNewGame()
+	{
+		System.out.println("----------------------------------------------");
+	}
+
+	private static void recreateGame()
+	{
+		System.out.println("test2");
 	}
 
 }
